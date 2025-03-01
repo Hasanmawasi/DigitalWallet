7a0b1eeb00caf9d1101b8b2c26ce35618e4c22a1
Hasanmawasi <hasanhasann@gmail.com>
2025-03-01 12:38:24 +0200

    connection file

---
+++ DigitalWallet-Server/connection/connection.php
@@ -0,0 +1,16 @@
+<?php 
+$servername = "localhost";
+$username="root";
+$password="";
+$db_name="digital_wallet";
+
+$mysqli = new mysqli($servername,$username,$password,$db_name);
+
+if($mysqli->connect_error){
+    echo"connection fail";
+}
+
+echo"connected successfully";
+
+
+?>
